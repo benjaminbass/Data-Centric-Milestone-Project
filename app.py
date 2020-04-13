e31c3f0dd26758cebf1cc6b0165de9c3c32a7861
Bendoo1 <bendoo12345@gmail.com>
2020-04-13 15:03:27 +0000

    Initial deployment

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'Categories'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def test():
+    return "<h3>Hello World!</h3>"
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
