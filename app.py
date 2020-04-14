c142248e7900f61d269b78c04832263790ac1742
Bendoo1 <bendoo12345@gmail.com>
2020-04-14 10:21:49 +0000

    Initial deployment, GitHub

--- app.py
+++ app.py
@@ -3,16 +3,30 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists('env.py'):
+    import env
+
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'Categories'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'Character'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def test():
-    return "<h3>Hello World!</h3>"
+@app.route('/get_characters')
+def get_characters():
+    return render_template("characters.html", characters=mongo.db.characterInfo.find(), classes=mongo.db.classes.find(), races=mongo.db.races.find())
+
+
+@app.route('/addcharacter')
+def add_character():
+    classes = list(mongo.db.classes.find())
+    races = list(mongo.db.races.find())
+    return render_template(
+        'addcharacter.html',
+        classes=classes,
+        races=races)
 
 
 if __name__ == '__main__':
